7c059113036bb07a8caa6f372a2fad5677874718
PBehague <114167055+QACS-TL@users.noreply.github.com>
2025-12-17 11:12:25 +0000

    Add files via upload

---
+++ CSharpToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoUtils;
+
+class Program
+{
+
+
+    static void Main()
+    {
+        while (true)
+        {
+            Utils.PrintMenu();
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Utils.ListTodos();
+            }
+            else if (choice == "2")
+            {
+                Utils.AddTodo();
+            }
+            else if (choice == "3")
+            {
+                Utils.RemoveTodo();
+            }
+            else if (choice == "4")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid choice");
+            }
+        }
+    }
+
+ 
+    
+}
---
+++ CSharpToDoApp/ToDoUtils/Utils.cs
@@ -0,0 +1,82 @@
+﻿namespace ToDoUtils
+{
+    public class Utils
+    {
+        // Global list of todos (equivalent to Python list)
+        static List<string> todos = new List<string> { "Make a cup of tea" };
+
+        public static void PrintTodos()
+        {
+            for (int i = 0; i < todos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {todos[i]}");
+            }
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("\nToDo Menu");
+            Console.WriteLine("1. View ToDos");
+            Console.WriteLine("2. Add a ToDo");
+            Console.WriteLine("3. Remove a ToDo");
+            Console.WriteLine("4. Exit");
+        }
+
+        public static void ListTodos()
+        {
+            if (todos.Count == 0)
+            {
+                Console.WriteLine("No ToDos!");
+            }
+            else
+            {
+                PrintTodos();
+            }
+        }
+
+        public static void AddTodo()
+        {
+            Console.Write("Enter a new todo: ");
+            string todo = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(todo))
+            {
+                Console.WriteLine("Entry cannot be empty!");
+            }
+            else
+            {
+                todos.Add(todo.Trim());
+                Console.WriteLine("Todo added!");
+            }
+        }
+
+        public static void RemoveTodo()
+        {
+            if (todos.Count == 0)
+            {
+                Console.WriteLine("No ToDos!");
+                return;
+            }
+
+            PrintTodos();
+            Console.Write("Enter a number to remove: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int num))
+            {
+                Console.WriteLine("Invalid number!");
+                return;
+            }
+
+            if (num <= 0 || num > todos.Count)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            else
+            {
+                string removedTodo = todos[num - 1];
+                todos.RemoveAt(num - 1);
+                Console.WriteLine($"Removed {removedTodo} from the list");
+            }
+        }
+    }
+}
